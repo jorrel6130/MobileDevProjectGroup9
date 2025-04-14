c81e8b221de99a0a34ea0bae2a07f5ecdae44155
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-04-14 16:26:59 -0400

    Created data controller for core data and connected to application

--- PrototypeScreens/PrototypeScreensApp.swift
+++ PrototypeScreens/PrototypeScreensApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct PrototypeScreensApp: App {
+    @StateObject private var dataController = DataController() // Creates live state of data controller
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
